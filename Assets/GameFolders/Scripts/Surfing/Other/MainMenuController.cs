6f7db2571248b71037c57242e4a8aa8cb8dc40c1
Onur Demircan <onurdemircan438@gmail.com>
2023-07-18 09:13:21 +0300

    MainMenu Continue Button

--- Assets/GameFolders/Scripts/Surfing/Other/MainMenuController.cs
+++ Assets/GameFolders/Scripts/Surfing/Other/MainMenuController.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] GameObject continueButton;
+    [SerializeField] TextMeshProUGUI continueButton;
 
     private void Awake()
     {
         if (PlayerPrefs.GetInt("ContinueGame") == 0)
-            continueButton.SetActive(false);
+            continueButton.alpha = 0.2f;
     }
 
     public void goFirstScene()
@@ -24,7 +25,10 @@ public class MainMenuController : MonoBehaviour
     public void continueScene()
     {
         //En son kayýt yapýlan sahneye git
-        SceneManager.LoadScene(PlayerPrefs.GetInt("LastBuildIndex", 1));
+        if(PlayerPrefs.GetInt("ContinueGame") != 0)
+        {
+            SceneManager.LoadScene(PlayerPrefs.GetInt("LastBuildIndex", 1));
+        }
     }
 
     public void goSecene(string sceneName)
